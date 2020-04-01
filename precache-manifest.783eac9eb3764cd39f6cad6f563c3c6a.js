861ef4bb6816bffb9902dac3e87a04d4fff3ca21
Mina UAE <mina.isaac.2000@gmail.com>
2020-04-01 14:41:19 +0200

    Updates

---
+++ precache-manifest.783eac9eb3764cd39f6cad6f563c3c6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9fd7940289e6a86ad81ce85fa131aa",
+    "url": "/roman-numerals/index.html"
+  },
+  {
+    "revision": "5b5fa3052b6b42a4f641",
+    "url": "/roman-numerals/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bad35b3147cdc4544a58",
+    "url": "/roman-numerals/static/js/2.784a495d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/roman-numerals/static/js/2.784a495d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b5fa3052b6b42a4f641",
+    "url": "/roman-numerals/static/js/main.bcddd6e9.chunk.js"
+  },
+  {
+    "revision": "19487f57f972185053d6",
+    "url": "/roman-numerals/static/js/runtime-main.760d86b8.js"
+  }
+]);
